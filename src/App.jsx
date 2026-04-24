d5941ee13848ef74909289dd3829a3b843a6693e
yogeshkumar <eyogeshkumar123@gmail.com>
2026-04-24 19:22:13 +0530

    Change header title from 'NovaDisk' to 'YOGESH's Disk'

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <SimulationProvider>
       <div className="container">
         <header>
-          <h1 className="gradient-text">NovaDisk</h1>
+          <h1 className="gradient-text">YOGESH's Disk</h1>
           <p>Advanced Disk Scheduling Algorithm Simulator</p>
         </header>
         
